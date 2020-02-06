9e3623ae6256225b14240f8002bb2528bef13297
james conant <jamesconant@jamess-MacBook-Pro.local>
2020-02-05 18:25:49 -0600

    heart functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,12 +8,14 @@ class App extends React.Component {
     super(props);
     this.state = {
       images: [],
+      heart: false,
       currentIndex: 0,
       mainImg: "",
       listingId: 758769542
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleNextClick = this.handleNextClick.bind(this);
+    this.handleHeartClick = this.handleHeartClick.bind(this);
   }
 
   componentDidMount() {
@@ -46,6 +48,10 @@ class App extends React.Component {
     });
   }
 
+  handleHeartClick() {
+    this.setState({ heart: !this.state.heart });
+  }
+
   getPreviousImage() {}
 
   handleClick(imageUrl, index) {
@@ -58,23 +64,21 @@ class App extends React.Component {
         {/* <div className="heart-button"></div> */}
         <div className="big-image">
           <img src={this.state.mainImg}></img>
-          <button className="change-buttons heart-button">
+          <button
+            className="change-buttons heart-button"
+            onClick={() => this.handleHeartClick()}
+          >
             <span
-              className="favorite-listing-button-icon-container icon-circle-container bg-white icon-group p-xs-1
-                    position-top-sm-down
-                    "
-              data-favorite-icon-container=""
+              className={this.state.heart ? "custom-display" : "heart-outline"}
             >
-              <span className="etsy-icon icon-t-1 text-gray display-block">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z"></path>
-                </svg>
-              </span>
-              {/* <span class="etsy-icon icon-t-1 text-red display-none">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z"></path>
-                </svg>
-              </span> */}
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z"></path>
+              </svg>
+            </span>
+            <span className={this.state.heart ? "heart-red" : "custom-display"}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z"></path>
+              </svg>
             </span>
           </button>
           <button
