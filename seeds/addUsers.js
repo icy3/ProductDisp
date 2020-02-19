541f94757d7463c7bebf5b30c0d928360e6ae4c9
Hack Reactor Student <student@hackreactor.com>
2020-02-19 12:41:30 -0600

    seeding faker with double for loop

--- seeds/addUsers.js
+++ seeds/addUsers.js
@@ -4,16 +4,13 @@ const createFakeInfo = () => ({
   images: faker.image.image(),
 })
           exports.seed = async function(knex, Promise) {
-            const fakeUsers = [];
-            const desiredFakeUsers =1001;
-            
-            var recurs = function(numy = 0) {
-              for(let i = 0; i<desiredFakeUsers; i++) {
+            const desiredFakeUsers =10000;
+            const numy = 1000
+            for (let j =0; j < numy; j++) {
+              const fakeUsers = [];
+                for(let i = 0; i<desiredFakeUsers; i++) {
                 fakeUsers.push(createFakeInfo());
               }
-              numy++;
-              if(numy < 3)
-              return recurs(numy)
             }
               await knex("users")
               .insert(fakeUsers)
