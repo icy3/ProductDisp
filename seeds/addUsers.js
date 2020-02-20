ae39f39f1394df3b1580a0a7b9bd6f3b87c93425
Hack Reactor Student <student@hackreactor.com>
2020-02-20 11:32:38 -0600

    seeded postgreSQL 11 million records

--- seeds/addUsers.js
+++ seeds/addUsers.js
@@ -3,15 +3,13 @@ const createFakeInfo = () => ({
   title: faker.commerce.productName(),
   images: faker.image.image(),
 })
-          exports.seed = async function(knex, Promise) {
-            const desiredFakeUsers =10000;
-            const numy = 1000
-            for (let j =0; j < numy; j++) {
-              const fakeUsers = [];
-                for(let i = 0; i<desiredFakeUsers; i++) {
-                fakeUsers.push(createFakeInfo());
-              }
-            }
-              await knex("users")
-              .insert(fakeUsers)
+exports.seed = async function (knex, Promise) {
+  let fakeUsers = []
+  for (let j = 0; j < 12000000; j++) {
+    fakeUsers.push(createFakeInfo())
+    if (j % 10000 === 0) {
+      await knex("users").insert(fakeUsers);
+      fakeUsers = []
+    }
+  }
 }
