32fa17240a61f3ac64e43ffb2a5333bd2f59487f
Hack Reactor Student <student@hackreactor.com>
2020-02-20 16:52:52 -0600

    update with mongoDB

---
+++ db/queries.js
@@ -0,0 +1,13 @@
+
+// configuration for the connection from server - database for postgress
+    // still need to alter many places to fit my 
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'student',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'student',
+  port: 5432,
+})
+const getImage = (request, response) => {
+}
